eed7048761a8125ebce195bc0a9fbf19a3fddbff
Christopher Jenkins <christj@gmail.com>
2016-05-03 17:32:48 -0700

    Manually install wine-mono. Use v3 of wix inside wine.

--- puppet/environments/production/manifests/site.pp
+++ puppet/environments/production/manifests/site.pp
@@ -1,22 +1,102 @@
-$target = '/usr/share/wix'
-$source = 'http://static.wixtoolset.org/releases/v4.0.3922.0/wix40-binaries.zip'
-
-file { 'WiX dir':
-  ensure => directory,
-  path   => $target,
-}
+$winetricks_src = 'https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks'
+$winetricks_dst = '/usr/local/bin/winetricks'
+$wine_mono_src = 'http://dl.winehq.org/wine/wine-mono/4.6.2/wine-mono-4.6.2.msi'
+$wine_mono_dst = '/var/tmp/wine-mono-4.6.2.msi'
+$wine_gecko_src = 'http://dl.winehq.org/wine/wine-gecko/2.44/wine_gecko-2.44-x86.msi'
+$wine_gecko_dst = '/var/tmp/wine_gecko-2.44-x86.msi'
 
 package { 'unzip': }
+package { 'xorg-x11-server-Xorg': }
+package { 'xauth': }
 package { 'epel-release': }
 ->
-package { ['mono-core','wine']: }
+package { 'wine': }
 
-staging::deploy { 'wix40-binaries.zip':
-  source  => $source,
-  target  => $target,
-  require => [
-    Package['unzip'],
-    File['WiX dir'],
-  ],
-  creates => "${target}/swc.exe",
+include display::xvfb
+
+staging::file {
+  'winetricks':
+    source => $winetricks_src,
+    target => $winetricks_dst,
+  ;
+  'wine-mono':
+    source => $wine_mono_src,
+    target => $wine_mono_dst,
+  ;
+  'wine-gecko':
+    source => $wine_gecko_src,
+    target => $wine_gecko_dst,
+  ;
 }
+
+file { 'winetricks':
+  path    => $winetricks_dst,
+  mode    => '0700',
+  require => Staging::File['winetricks'],
+}
+
+exec { 'Install DotNet 4.5':
+  environment =>
+  [
+    'DISPLAY=:0.0',
+    'WINEDLLOVERRIDES=mscoree,mshtml=',
+    'WINEPREFIX=/var/tmp',
+  ],
+  command     => "${winetricks_dst} -q dotnet20",
+  require     =>
+  [
+    File['winetricks'],
+    Package['wine'],
+    Class['display::xvfb'],
+  ],
+  logoutput   => true,
+  timeout     => 0,
+  #unless      => "${winetricks_dst} | grep -q dotnet20",
+  unless      => "/bin/true",
+}
+->
+exec { 'Install wine-mono':
+  environment =>
+  [
+    'DISPLAY=:0.0',
+    'WINEDLLOVERRIDES=mscoree,mshtml=',
+    'WINEPREFIX=/var/tmp',
+  ],
+  require     =>
+  [
+    Staging::File['wine-mono'],
+    Class['display::xvfb'],
+  ],
+  command     => "/usr/bin/msiexec /i /p ${wine_mono_dst}",
+  logoutput   => true,
+  timeout     => 0,
+}
+# exec { 'Install wine-gecko':
+
+#   environment =>
+
+#   [
+
+#     'DISPLAY=:0.0',
+
+#     'WINEDLLOVERRIDES=mscoree,mshtml=',
+
+#   ],
+
+#   require     =>
+
+#   [
+
+#     Staging::File['wine-gecko'],
+
+#     Class['display::xvfb'],
+
+#   ],
+
+# command     => "/usr/bin/msiexec /i /p ${wine_gecko_dst}",
+
+#   logoutput   => true,
+
+#   timeout     => 0,
+
+# }
